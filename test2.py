90b5201bc7bb88be591c7eb3f64ebef062b28a71
PrajaktaMaid <99666612+PrajaktaMaid@users.noreply.github.com>
2022-02-22 09:22:50 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,9 +1,9 @@
-# Store input numbers
-num1 = input('Enter first number: ')
-num2 = input('Enter second number: ')
+# Python String Operations
+str1 = 'Hello'
+str2 ='World!'
 
-# Add two numbers
-sum = float(num1) + float(num2)
+# using +
+print('str1 + str2 = ', str1 + str2)
 
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+# using *
+print('str1 * 3 =', str1 * 3)
